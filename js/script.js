8c814dfd8579e97b420d8b47042640842d46971b
Sharath-29 <sharathshalla17@gmail.com>
2021-11-06 14:16:34 +0530

    Final UI Update

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".card-input").payment("formatCardNumber");
-  $(".expiry-input").payment("formatCardExpiry");
-  $(".cvc-input").payment("formatCardCVC");
-
-  $(".form").on("submit", function (e) {
-    e.preventDefault();
-  });
-});
